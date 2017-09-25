ca11e2d9a16e44c5e0fab26c2e5b1af8b461ae76
Hannah Danziger <hdanziger8@gmail.com>
2017-09-25 17:27:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,15 +18,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash.each do |season, holiday|
-    holiday.each do |attribute, value|
-      if attribute == :fourth_of_july
-        value.each do |activity|
-          puts "#{activity}"
-        end
-      end
-    end
-  end
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
