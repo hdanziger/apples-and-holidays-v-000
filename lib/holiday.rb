59a4542d64cd3f399041ff8e02d6f3911b381943
Hannah Danziger <hdanziger8@gmail.com>
2017-09-25 17:47:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, items|
-      puts "#{holiday.capitalize}: #{items.capitalize}"
+      puts "#{holiday}: #{items}"
     end
   end
 end
